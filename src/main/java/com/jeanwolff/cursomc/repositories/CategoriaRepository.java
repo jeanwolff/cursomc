b2b518a3fb55e9c3535c4d15ee2df3b36776e5de
Jean Wolff <wolff.jean@live.com>
2018-05-22 13:24:06 -0300

    Criando repository e service para Categoria

---
+++ src/main/java/com/jeanwolff/cursomc/repositories/CategoriaRepository.java
@@ -0,0 +1,13 @@
+package com.jeanwolff.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.jeanwolff.cursomc.domain.Categoria;
+
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
+
+	
+}
