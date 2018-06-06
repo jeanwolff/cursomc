4ef9c7a9a7000f836cfc0cd21b0fa8cb5f433e8e
Jean Wolff <wolff.jean@live.com>
2018-06-05 23:49:10 -0300

    Listando todas as Categorias com DTO

---
+++ src/main/java/com/jeanwolff/cursomc/dto/CategoriaDTO.java
@@ -0,0 +1,90 @@
+package com.jeanwolff.cursomc.dto;
+
+import java.io.Serializable;
+
+import com.jeanwolff.cursomc.domain.Categoria;
+
+public class CategoriaDTO implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+
+	public CategoriaDTO() {
+		super();
+	}
+
+	public CategoriaDTO(Categoria categoria) {
+		super();
+		this.id = categoria.getId();
+		this.nome= categoria.getNome();
+	}
+	
+	public CategoriaDTO(Integer id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+	}
+
+	private Integer id;
+	
+	private String nome;
+
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((nome == null) ? 0 : nome.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CategoriaDTO other = (CategoriaDTO) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (nome == null) {
+			if (other.nome != null)
+				return false;
+		} else if (!nome.equals(other.nome))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "CategoriaDTO [id=" + id + ", nome=" + nome + "]";
+	}
+	
+	
+	
+}
