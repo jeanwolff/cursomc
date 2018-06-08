158393f6ed999ab88d1d80bb879e85be2ce35a2c
Jean Wolff <wolff.jean@live.com>
2018-06-07 21:31:53 -0300

    Personalizando o retorno da Validação.

---
+++ src/main/java/com/jeanwolff/cursomc/Constants.java
@@ -0,0 +1,7 @@
+package com.jeanwolff.cursomc;
+
+public class Constants {
+
+	
+	public static final String ERRO_VALIDACAO = "Erro de Validação";
+}
--- src/main/java/com/jeanwolff/cursomc/resources/exceptions/ResourceExceptionHandler.java
+++ src/main/java/com/jeanwolff/cursomc/resources/exceptions/ResourceExceptionHandler.java
@@ -6,9 +6,12 @@ import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.jeanwolff.cursomc.Constants;
 import com.jeanwolff.cursomc.services.exceptions.DataIntegrityException;
 import com.jeanwolff.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -29,5 +32,16 @@ public class ResourceExceptionHandler {
 		StandardError err = new StandardError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), new Date(System.currentTimeMillis()) );
 		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
 	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<StandardError> validation(MethodArgumentNotValidException e, HttpServletRequest request) {
+		ValidationError err = new ValidationError(HttpStatus.BAD_REQUEST.value(), Constants.ERRO_VALIDACAO, new Date(System.currentTimeMillis()) );
+ 		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+			err.addError(x.getField(), x.getDefaultMessage());
+		}
+		
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(err);
+	}
+	
 
 }
