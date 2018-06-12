c32cbfbb874ce09843a2e9f1cb9702b06c0c388e
Jean Wolff <wolff.jean@live.com>
2018-06-11 23:09:31 -0300

    Inserindo um novo Cliente com POST

--- src/main/java/com/jeanwolff/cursomc/resources/ClienteResource.java
+++ src/main/java/com/jeanwolff/cursomc/resources/ClienteResource.java
@@ -1,5 +1,6 @@
 package com.jeanwolff.cursomc.resources;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,9 +15,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.jeanwolff.cursomc.domain.Cliente;
 import com.jeanwolff.cursomc.dto.ClienteDTO;
+import com.jeanwolff.cursomc.dto.ClienteNewDTO;
 import com.jeanwolff.cursomc.services.ClienteService;
 
 @RestController
@@ -65,5 +68,14 @@ public class ClienteResource {
 		return ResponseEntity.ok().body(listDto);
 	}
 
+	@RequestMapping(method = RequestMethod.POST)
+	public ResponseEntity<Void> insert(@Valid @RequestBody ClienteNewDTO clienteNewDTO){
+		Cliente cliente = service.fromDTO(clienteNewDTO);
+		cliente = service.insert(cliente);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(cliente.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
+	
 	
 }
--- src/main/java/com/jeanwolff/cursomc/services/ClienteService.java
+++ src/main/java/com/jeanwolff/cursomc/services/ClienteService.java
@@ -10,9 +10,14 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
+import com.jeanwolff.cursomc.domain.Cidade;
 import com.jeanwolff.cursomc.domain.Cliente;
+import com.jeanwolff.cursomc.domain.Endereco;
+import com.jeanwolff.cursomc.domain.enums.TipoCliente;
 import com.jeanwolff.cursomc.dto.ClienteDTO;
+import com.jeanwolff.cursomc.dto.ClienteNewDTO;
 import com.jeanwolff.cursomc.repositories.ClienteRepository;
+import com.jeanwolff.cursomc.repositories.EnderecoRepository;
 import com.jeanwolff.cursomc.services.exceptions.DataIntegrityException;
 import com.jeanwolff.cursomc.services.exceptions.ObjectNotFoundException;
 
@@ -22,6 +27,9 @@ public class ClienteService {
 
 	@Autowired
 	private ClienteRepository repo;
+	
+	@Autowired
+	private EnderecoRepository enderecoRepository;
 
 	public Cliente find(Integer id) {
 		Optional<Cliente> obj = repo.findById(id);
@@ -30,7 +38,9 @@ public class ClienteService {
 
 	public Cliente insert(Cliente cliente) {
 		cliente.setId(null);
-		return repo.save(cliente);
+		cliente = repo.save(cliente);
+		enderecoRepository.saveAll(cliente.getEnderecos());
+		return cliente;
 	}
 
 	public Cliente update(Cliente cliente) {
@@ -59,6 +69,23 @@ public class ClienteService {
 	
 	public Cliente fromDTO(ClienteDTO clienteDTO) {
 		return new Cliente(clienteDTO.getId(), clienteDTO.getNome(), clienteDTO.getEmail(), null, null);
+	}
+	
+	public Cliente fromDTO(ClienteNewDTO clienteNewDTO) {
+		Cliente cliente = new Cliente(clienteNewDTO.getNome(), clienteNewDTO.getEmail(), clienteNewDTO.getCpfOuCnpj(), TipoCliente.toEnum(clienteNewDTO.getTipo()));
+		Cidade cidade = new Cidade(clienteNewDTO.getCidadeId(), null, null);
+		Endereco end = new Endereco(clienteNewDTO.getLogradouro(), clienteNewDTO.getNumero(), clienteNewDTO.getComplemento(), clienteNewDTO.getBairro(), clienteNewDTO.getCep(), cliente, cidade);
+		cliente.getEnderecos().add(end);
+		cliente.getTelefones().add(clienteNewDTO.getTelefone1());
+		
+		if(clienteNewDTO.getTelefone2()!=null) {
+			cliente.getTelefones().add(clienteNewDTO.getTelefone2());
+		}
+		if(clienteNewDTO.getTelefone3()!=null) {
+			cliente.getTelefones().add(clienteNewDTO.getTelefone3());
+		}
+		
+		return cliente;
 	}
 	
 	private void updateData(Cliente newCliente, Cliente cliente) {
