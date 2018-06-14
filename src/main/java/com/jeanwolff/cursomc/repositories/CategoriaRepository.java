7646e653b8b6717673b03d410499b4924005aa7b
Jean Wolff <wolff.jean@live.com>
2018-06-14 14:15:02 -0300

    Busca de Produtos por nome e Categorias.

--- src/main/java/com/jeanwolff/cursomc/repositories/CategoriaRepository.java
+++ src/main/java/com/jeanwolff/cursomc/repositories/CategoriaRepository.java
@@ -1,13 +1,19 @@
 package com.jeanwolff.cursomc.repositories;
 
+
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jeanwolff.cursomc.domain.Categoria;
 
 
 @Repository
+@Transactional(readOnly = true)
 public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
 
+	
+	
 	
 }
